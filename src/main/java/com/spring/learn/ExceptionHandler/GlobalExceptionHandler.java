6fc071f86e47025a79a822f5e617e1eb15850ec3
Amit-1997 <amitchaurasia1997.ac@gmail.com>
2024-03-30 12:44:10 +0530

    added Global exception hadeling feature

---
+++ src/main/java/com/spring/learn/ExceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.spring.learn.ExceptionHandler;
+
+
+import com.spring.learn.Exception.DuplicateValueFoundException;
+import com.spring.learn.Exception.ResourceNotFoundException;
+import com.spring.learn.pojo.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex)
+    {
+    String message=ex.getMessage();
+    ApiResponse apiResponse=new ApiResponse(message,false);
+
+    return new ResponseEntity<ApiResponse>(apiResponse,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DuplicateValueFoundException.class)
+    public ResponseEntity<ApiResponse> duplicateValueFoundExceptionHandler(DuplicateValueFoundException ex)
+    {
+        String message=ex.getMessage();
+        ApiResponse apiResponse=new ApiResponse(message,false);
+
+        return new ResponseEntity<ApiResponse>(apiResponse,HttpStatus.ALREADY_REPORTED);
+    }
+}
